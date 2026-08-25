a2fd03ce3c6b26bafe5b9805f6e621cf810d9c65
DirectorofWealth <reverencewealth005@gmail.com>
2026-08-25 16:23:10 +0100

    feature/Layout

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,18 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import { BrowserRouter, Routes, Route } from "react-router";
+import Layout from "./layout/layout";
 
 const root = document.getElementById("root");
 
 ReactDOM.createRoot(root).render(
-  <BrowserRouter>
-    <Routes>
-      <Route path="/" element={<App />} />
-    </Routes>
-  </BrowserRouter>,
+  <Routes>
+    <Route element={<Layout />}>
+      <Route index element={<Home />} />
+      <Route path="product" element={<Product />} />
+      <Route path="productdetails" element={<Pro />} />
+      <Route path="notfound" element={<Home />} />
+    </Route>
+    
+</Routes>
 );
